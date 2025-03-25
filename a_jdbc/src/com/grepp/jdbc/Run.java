2893e411f745cf0ad638cb69dc45dcc248686976
imseohyeon <forestlight749@gmail.com>
2025-03-26 00:43:04 +0900

    (0324) init

---
+++ a_jdbc/src/com/grepp/jdbc/Run.java
@@ -0,0 +1,12 @@
+package com.grepp.jdbc;
+
+// NOTE 01 JDBC API
+// JDBC : Java Database Connectivity
+// API : Application Programming Interface
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+
+}
