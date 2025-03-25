05c54479a3a0b34be6288a3a1cd691a7cca33357
imseohyeon <forestlight749@gmail.com>
2025-03-26 01:45:12 +0900

    (0324) CRUD

--- a_jdbc/src/com/grepp/jdbc/Run.java
+++ a_jdbc/src/com/grepp/jdbc/Run.java
@@ -1,12 +1,49 @@
 package com.grepp.jdbc;
 
+import com.grepp.jdbc.member.code.Grade;
+import com.grepp.jdbc.member.dao.MemberDao;
+import com.grepp.jdbc.member.dto.MemberDto;
+
 // NOTE 01 JDBC API
 // JDBC : Java Database Connectivity
 // API : Application Programming Interface
 public class Run {
 
     public static void main(String[] args) {
+        MemberDao dao = new MemberDao();
+//        insert(dao);
+//        select(dao);
+        update(dao);
+//        delete(dao);
+    }
 
+    private static void delete(MemberDao dao) {
+        MemberDto dto = new MemberDto();
+        dto.setUserId("test");
+        System.out.println(dao.delete(dto));
+    }
+
+    private static void update(MemberDao dao) {
+        MemberDto dto = new MemberDto();
+        dto.setUserId("super");
+        dto.setPassword("1111");
+        System.out.println(dao.update(dto));
+    }
+
+    private static void select(MemberDao dao) {
+        System.out.println(dao.selectByIdAndPassword("super", "9999"));
+    }
+
+    private static void insert(MemberDao dao) {
+        MemberDto dto = new MemberDto();
+        dto.setUserId("lsh");
+        dto.setPassword("9999");
+        dto.setEmail("super@gmail.com");
+        dto.setTell("010-2222-3333");
+        dto.setLeave(false);
+        dto.setGrade(Grade.ROLE_ADMIN);
+
+        System.out.println(dao.insert(dto));
     }
 
 }
