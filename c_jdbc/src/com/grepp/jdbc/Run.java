6c318187321c634ce26dcd84718e950d790d66d8
imseohyeon <forestlight749@gmail.com>
2025-03-26 11:19:23 +0900

    (0326) init

--- c_jdbc/src/com/grepp/jdbc/Run.java
+++ c_jdbc/src/com/grepp/jdbc/Run.java
@@ -5,6 +5,7 @@ import com.grepp.jdbc.app.member.dao.MemberDao;
 import com.grepp.jdbc.app.member.dto.MemberDto;
 import com.grepp.jdbc.infra.db.JdbcTemplate;
 import com.grepp.jdbc.infra.exception.DataAccessException;
+import com.grepp.jdbc.view.Index;
 import java.util.Optional;
 
 // NOTE 01 JDBC API
@@ -14,63 +15,16 @@ public class Run {
 
     public static void main(String[] args) {
 
-        try{
+        try {
             String url = "jdbc:mysql://localhost:3306/jdbc?useUnicode=true&characterEncoding=utf8";
             String user = "bm";
             String password = "it2116016^^";
 
             JdbcTemplate.init(url, user, password);
 
-            MemberDao dao = new MemberDao();
-//            insert(dao);
-            select(dao);
-//            update(dao);
-//            delete(dao);
-        }catch (DataAccessException e){
+            new Index().menu();
+        } catch (DataAccessException e) {
             e.printStackTrace();
         }
-
-        System.out.println("끝!!");
     }
-
-    private static void delete(MemberDao dao) {
-        MemberDto dto = new MemberDto();
-        dto.setUserId("test");
-        System.out.println(dao.delete(dto));
-    }
-
-    private static void update(MemberDao dao) {
-        MemberDto dto = new MemberDto();
-        dto.setUserId("super");
-        dto.setPassword("9999");
-
-        Optional<MemberDto> res = dao.update(dto);
-        res.ifPresent(System.out::println);
-    }
-
-    private static void select(MemberDao dao) {
-        Optional<MemberDto> res = dao.selectByIdAndPassword("super", "9999");
-
-        if(res.isPresent()) {
-            System.out.println(res.get());
-            return;
-        }
-
-        System.out.println("존재하지 않는 사용자 입니다.");
-
-    }
-
-    private static void insert(MemberDao dao) {
-        MemberDto dto = new MemberDto();
-        dto.setUserId("test");
-        dto.setPassword("9999");
-        dto.setEmail("test@gmail.com");
-        dto.setTell("010-2222-3333");
-        dto.setLeave(false);
-        dto.setGrade(Grade.ROLE_ADMIN);
-
-        Optional<MemberDto> res = dao.insert(dto);
-        res.ifPresent(System.out::println);
-    }
-
 }
